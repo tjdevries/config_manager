353cd74701fd21489745a65c67864f10d58bc609
TJ DeVries <timothydvrs1234@gmail.com>
2015-10-12 16:21:23 -0400

    Start to add support for compiling custom vim

--- vim/install_vim.sh
+++ vim/install_vim.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
 
+# Check for the configuration items that we want
+if [ ! "$(vim --version | grep +python3 )" ]; then
+    need_compile=1
+fi
+
+if [ $need_compile ]; then
+    # Compile vim with the things that I like
+    # Get the required development packages 
+    sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+        libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+        libcairo2-dev libx11-dev libxpm-dev libxt-dev -y
+
+    sudo apt-get install ruby-dev python3-dev -y
+
+    # Got to home directory and get the git repository
+    cd ~
+    git clone https://github.com/b4winckler/vim
+    git clone https://github.com/drewinglis/vim-breakindent ~/Downloads/
+
+    cd ~/vim
+    patch -p1 < ~/Downloads/vim-breakindent/breakindent.patch
+
+    # Begin the make process
+    make distclean
+
+    # Configuration options here
+    ./configure \
+        --enable-perlinterp \
+        --enable-python3interp=yes \
+        --with-python3-config-dir="$(python3-config --configdir)" \
+        --enable-rubyinterp \
+        --enable-cscope \
+        --enable-gui=auto \
+        --enable-gtk2-check \
+        --enable-gnome-check \
+        --with-features=huge \
+        --enable-multibyte \
+        --with-x \
+        --with-compiledby="xorpd" \
+        --prefix=/opt/vim74
+
+    make
+    sudo make install
+fi
+
 # First we need to install pathogen
 if [ ! -d ~/.vim/autoload ]; then
     mkdir -p ~/.vim/autoload ~/.vim/bundle && \
