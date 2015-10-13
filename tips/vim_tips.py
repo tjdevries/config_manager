188ab31e0ff25f460576c20d01a6b1cecd1b263c
TJ DeVries <timothydvrs1234@gmail.com>
2015-10-12 22:26:24 -0400

    Add another tip

--- tips/vim_tips.py
+++ tips/vim_tips.py
@@ -6,7 +6,8 @@ import random
 # Define the tips
 tips = ["The last command entered with ':' can be repeated with @: and further repeats can be done with @@",
         "Using the * key searches for the word under the cursor. Use # to do the same thing backwards",
-        "`:Tab /{Pattern}` will align the current paragraph on that pattern"
+        "`:Tab /{Pattern}` will align the current paragraph on that pattern",
+        "<C-p> will complete words that are used in your current file",
         ]
 
 # Print a random tip!
