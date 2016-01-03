738c1bd26310f0d485ddf36f4303e545d0a063f1
TJ DeVries <devries.timothyj@gmail.com>
2016-01-02 23:29:31 -0500

    Add another VIM tip

--- tips/vim_tips.py
+++ tips/vim_tips.py
@@ -8,7 +8,9 @@ tips = ["The last command entered with ':' can be repeated with @: and further r
         "Using the * key searches for the word under the cursor. Use # to do the same thing backwards",
         "`:Tab /{Pattern}` will align the current paragraph on that pattern",
         "<C-p> will complete words that are used in your current file",
-        "`:verbose set <name>?` tells you where a variable was set"
+        "`:verbose set <name>?` tells you where a variable was set",
+        "<C-a> will increment a number that the cursor is over",
+        "<C-x> will decrement a number that the cursor is over"
         ]
 
 # Print a random tip!
