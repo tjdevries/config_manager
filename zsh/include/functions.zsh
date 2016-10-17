095e60f7d85f5ee4e5fda08dfc53583a438cc8ed
TJ DeVries <tim.devries@dornerworks.com>
2016-10-17 10:46:48 -0400

    Add diff helper

--- zsh/include/functions.zsh
+++ zsh/include/functions.zsh
@@ -1,3 +1,12 @@
 print_all_the_colors() {
     for code in {000..255}; do print -P -- "$code: %F{$code}Test%f"; done
 }
+
+diff_commit() {
+    if [ "$1" != "" ]
+    then
+        git diff $1~ $1
+    else
+        git diff HEAD~ HEAD
+    fi
+}
