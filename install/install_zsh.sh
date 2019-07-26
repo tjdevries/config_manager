1704f47eda77c78596373281ffc7988dfe5c1cb7
TJ DeVries <devries.timothyj@gmail.com>
2019-07-26 10:15:33 -0400

    Easier updates

---
+++ install/install_zsh.sh
@@ -0,0 +1,12 @@
+sudo apt install zsh
+
+chsh -s $(which zsh)
+
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install spaceship theme
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
