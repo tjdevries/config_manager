0d41dc2bb90770092f3a6385709fe09c44f13672
Michael Vessia <michael@vessia.net>
2022-12-09 11:00:47 -0500

    Update init.lua

--- xdg_config/nvim/init.lua
+++ xdg_config/nvim/init.lua
@@ -62,7 +62,7 @@ if require "tj.first_load"() then
 end
 
 if vim.g.neovide then
-  vim.g.neovide_cursor_trail_legnth = 0
+  vim.g.neovide_cursor_trail_length = 0
   vim.g.neovide_cursor_animation_length = 0
   vim.o.guifont = "Jetbrains Mono"
 end
